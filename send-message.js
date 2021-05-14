098a85809eaa2782d98787fcdb9b0676266793e3
Rostyslav <rostikgtun@gmail.com>
2021-05-14 08:46:38 +0300

    Add access token to headers of requests to api

--- send-message.js
+++ send-message.js
@@ -6,6 +6,15 @@ module.exports = function(RED) {
     //for making requests
     const axios = require('axios');
 
+    function strIsJson(str) {
+      try {
+        JSON.parse(str);
+      } catch (e) {
+        return false;
+      }
+      return true;
+    }
+
     //if 'FB_PAGE_TOKEN' var doesn't exist in envs
     if (!process.env.FB_PAGE_TOKEN) {
       return node.error("Please add Facebook page token (for gettings access) variable (as FB_PAGE_TOKEN) to envs!");
@@ -30,6 +39,8 @@ module.exports = function(RED) {
     if (!process.env.API_URL) {
       return node.error("Please add api url variable (as API_URL) to envs!");
     }
+
+
 
     //node 'input' event handler
     node.on('input', function (msg) {
@@ -68,20 +79,25 @@ module.exports = function(RED) {
         msg
       );
 
-      
+
 
       //get message
       axios
-        .get(`${process.env.API_URL}/messages?templateId=${templateId}&messenger_type=${messenger_type}`)
+        .get(`${process.env.API_URL}/messages?templateId=${templateId}&messenger_type=${messenger_type}`, {
+          "headers": {'Authorization': `Bearer ${process.env.API_TOKEN}`}
+        })
         .then((response) => {
           let message = JSON.stringify(response.data.json);
 
           //set template vars
-          if (!response.data.hasOwnProperty(language)) return node.error("Template does not support this language.");
-          let message_vars = msg.template_vars ? Object.assign(msg.template_vars, response.data[language]) : response.data[language];
+          let message_vars = (msg.template_vars && response.data[language]) ? Object.assign(msg.template_vars, response.data[language]) : (msg.template_vars ? msg.template_vars : (response.data[language] ? response.data[language] : {}));
 
           for (let key in message_vars) {
-            message = message.replace(new RegExp("{{" + key + "}}", "g"), message_vars[key]);
+            if (!strIsJson(message_vars[key])) {
+              message = message.replace(new RegExp("{{" + key + "}}", "g"), message_vars[key]);
+            } else {
+              message = message.replace(new RegExp("\"{{" + key + "}}\"", "g"), message_vars[key]);
+            }
           };
 
           message = JSON.parse(message);
@@ -89,6 +105,11 @@ module.exports = function(RED) {
 
 
           //https request to send message
+
+          //creating request options
+          let headers = {
+            'Content-Type': 'application/json'
+          };
 
           //crating request data
           if (messenger_type == "fb") {
@@ -99,40 +120,53 @@ module.exports = function(RED) {
           } else if (messenger_type == "vb") {
             var send_message_url = "https://chatapi.viber.com/pa/send_message";
 
+            //adding token for viber
+            headers['X-Viber-Auth-Token'] = process.env.VB_TOKEN;
+
             message["sender"] = {
               "name": process.env.VB_BOT_NAME,
               "avatar": process.env.VB_AVATAR_URL
             };
             message["receiver"] = recipient_id;
           } else if (messenger_type == "tg") {
-            var send_message_url = `https://api.telegram.org/bot${process.env.TG_TOKEN}/sendMessage`;
+            var action = "sendMessage";
+
+            if (message.hasOwnProperty("photo")) {
+              var action = "sendPhoto"
+            }
+
+            var send_message_url = `https://api.telegram.org/bot${process.env.TG_TOKEN}/${action}`;
 
             message["chat_id"] = recipient_id;
           }
+          console.log(message);
+          console.log(send_message_url);
+          console.log(headers);
 
-          //creating request options
-          let headers = {
-            'Content-Type': 'application/json'
-          };
 
-          //adding token for viber
-          if (config.messenger_type == "vb") {
-            headers['X-Viber-Auth-Token'] = process.env.VB_TOKEN;
-          }
 
           //making the request
+          msg.payload = {};
           axios
             .post(send_message_url, message, { headers })
             .then((res) => {
               //sending response
-              msg.payload = res;
-              node.send(msg);
+              msg.payload.send_message_response = res;
 
               //change user state
               axios
-                .put(`${process.env.API_URL}/users?user_id=${recipient_id}`, {
+                .put(`${process.env.API_URL}/users?user_id=${encodeURIComponent(recipient_id)}`, {
                     "state": templateId
-                  }, headers)
+                  }, {
+                    "headers": {
+                      'Content-Type': 'application/json',
+                      'Authorization': `Bearer ${process.env.API_TOKEN}`
+                    }
+                  })
+                .then((new_res) => {
+                  //sending response
+                  msg.payload.update_user_state_response = new_res;
+                })
                 .catch((error) => {
                   node.error("Setting user state error: " + error);
                 });
@@ -140,6 +174,11 @@ module.exports = function(RED) {
             .catch((error) => {
               node.error("Sending message error: " + error);
             });
+
+            node.send(msg);
+        })
+        .catch((error) => {
+          node.error("Getting message error: " + error);
         });
     });
   }
